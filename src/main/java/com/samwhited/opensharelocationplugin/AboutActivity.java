69066fcc41c5b851f057387294cb516a142480ef
Sam Whited <sam@samwhited.com>
2015-03-15 00:02:46 -0500

    Add missing files... oops.

---
+++ src/main/java/com/samwhited/opensharelocationplugin/AboutActivity.java
@@ -0,0 +1,35 @@
+package com.samwhited.opensharelocationplugin;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		final ActionBar actionBar = getActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+
+		setContentView(R.layout.activity_about);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ src/main/java/com/samwhited/opensharelocationplugin/Config.java
@@ -0,0 +1,27 @@
+package com.samwhited.opensharelocationplugin;
+
+import android.location.Location;
+import android.net.Uri;
+
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+
+public final class Config {
+	public final static int INITIAL_ZOOM_LEVEL = 4;
+	public final static int FINAL_ZOOM_LEVEL = 15;
+	public final static GeoPoint INITIAL_POS = new GeoPoint(33.805278, -84.171389);
+	public final static int LOCATION_FIX_TIME_DELTA = 0;
+	public final static int LOCATION_FIX_SPACE_DELTA = 0;
+	public final static int LOCATION_FIX_SIGNIFICANT_TIME_DELTA = 1000 * 60 * 2; // Two minutes
+	public final static ITileSource TILE_SOURCE_PROVIDER = TileSourceFactory.MAPNIK;
+
+	public static Uri createGeoUri(final Location location) {
+		return createGeoUri(new GeoPoint(location));
+	}
+
+	public static Uri createGeoUri(final GeoPoint location) {
+		return Uri.parse("geo:" + location.getLatitude() + "," + location.getLongitude());
+	}
+
+}
